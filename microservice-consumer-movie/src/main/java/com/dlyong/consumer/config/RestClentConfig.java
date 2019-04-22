1b486735d70248cf0bced541ac607981c0a8f501
zhangyong <zhangyong@zlonmarine.com>
2019-04-22 10:38:33 +0800

    通过eureka服务，修改服务消费者ctrl

--- microservice-consumer-movie/src/main/java/com/dlyong/consumer/config/RestClentConfig.java
+++ microservice-consumer-movie/src/main/java/com/dlyong/consumer/config/RestClentConfig.java
@@ -2,6 +2,7 @@ package com.dlyong.consumer.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -11,6 +12,7 @@ public class RestClentConfig {
 
     @Autowired
     private RestTemplateBuilder restTemplateBuilder;
+
     @Bean
     public RestTemplate restTemplate() {
 
--- microservice-consumer-movie/src/main/java/com/dlyong/consumer/controller/MovieController.java
+++ microservice-consumer-movie/src/main/java/com/dlyong/consumer/controller/MovieController.java
@@ -1,9 +1,7 @@
 package com.dlyong.consumer.controller;
 
-import com.fasterxml.jackson.databind.util.JSONPObject;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,14 +17,14 @@ public class MovieController {
     @Autowired
     private RestTemplate restTemplate;
 
-    @Value("${provider.providerServiceUrl}")
-    private String providerServiceUrl;
-
     @GetMapping("/user/{id}")
-    public String getProviderServiceUserInfoById_2(@PathVariable("id") String id) {
-        log.info("客户端调取服务端的方法:通过value注解获取配置文件中服务端URL的方式");
+    public String getProviderUser(@PathVariable("id") String id) {
+
+        return restTemplate.getForObject("microservice-provider-user:8000/"+id,String.class);
+
+      /*  log.info("客户端调取服务端的方法:通过value注解获取配置文件中服务端URL的方式");
         log.info("服务端Url:"+providerServiceUrl);
-        return  restTemplate.getForObject(providerServiceUrl + id,String.class);
+        return  restTemplate.getForObject(providerServiceUrl + id,String.class);*/
     }
 
 }
